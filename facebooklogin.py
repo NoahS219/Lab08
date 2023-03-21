f88e7d3a019d667041184d167fa307873d135d1e
Noah Santagata <noah_santagata@uri.edu>
2023-03-21 16:18:00 -0400

    Code for the facebook login

---
+++ facebooklogin.py
@@ -0,0 +1,8 @@
+userName = input("Hello! Please enter your facebook username! ")
+
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "noah" and userPassword == "Password123"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
